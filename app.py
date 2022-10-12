a4068956f171803befb61f9a95ea278e5ee9c73e
michael-bocim <mtbocim@gmail.com>
2022-10-12 17:10:48 -0600

    Page loads

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def show_form_fields():
+    prompts = silly_story.prompts
+    return render_template("questions.html", prompts=prompts)
