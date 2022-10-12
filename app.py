fdab348ece34ed1129e8bb46c893aa94360da1c1
Ashley Mathew <amathew195@gmail.com>
2022-10-12 18:49:13 -0500

    can generate and display story

--- app.py
+++ app.py
@@ -11,5 +11,22 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def show_form_fields():
+    """This function updates the questions html to show the appropriate inputs
+    needed based on the story text"""
     prompts = silly_story.prompts
-    return render_template("questions.html", prompts=prompts)
+    return render_template("questions.html", prompts = prompts)
+
+@app.get("/story")
+def show_story():
+    """This function collects the form data. Using data, it generates and
+    displays the story on the results.html"""
+    inputs = {}
+
+    for prompt in silly_story.prompts:
+        response = request.args[prompt]
+        inputs[prompt] = response
+
+    story = silly_story.generate(inputs)
+
+    return render_template("results.html", story = story)
+
